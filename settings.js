5c715f14f9a4b3de4d250944b35b820569c9386b
LUCIXSINU07 <bdreem78@gmail.com>
2024-12-19 19:06:58 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const version = require("@whiskeysockets/baileys/package.json").version
 //Input number for pair code
-global.pairingNumber = "2349019529423"
+global.pairingNumber = "919674673966"
 
-global.botNumber = ["2349019529423"]
+global.botNumber = ["919674673966"]
 
 global.anticall = false
 
